6c59572ec9d82e8635b37480b2329cf726009849
Muchezz <muchezz@outlook.com>
2020-08-28 12:32:37 +0300

    Database

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,14 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Kaehcle'}
+   
+    return render_template('index.html', title='Home', user=user)
 
-    return render_template('/index.html', title='Home', user=user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -16,4 +18,4 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html',  title='Sign In', form=form)
